312b78ed25b5b39ee8c088fd7671968151773c2b
lokesh4877saini <genu08saini@gmail.com>
2025-07-05 22:32:34 +0530

    initia ai-chatbot

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -3,7 +3,7 @@
 import { createContext, useContext, useState,useEffect } from 'react';
 
 export const AuthContext = createContext();
-// 
+ 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
