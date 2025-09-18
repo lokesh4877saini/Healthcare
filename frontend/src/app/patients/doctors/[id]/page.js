d0bdf0d24c60836b375eb50c7e719d1c1f49294a
lokesh4877saini <genu08saini@gmail.com>
2025-09-18 18:15:29 +0530

    update ui

--- frontend/src/app/patients/doctors/[id]/page.js
+++ frontend/src/app/patients/doctors/[id]/page.js
@@ -1,15 +1,8 @@
 // src/app/patients/doctors/[id]/page.js
 import { fetcher } from '@/lib/api';
-import styles from '@/styles/DoctorDetailPage.module.css';
-import { getCurrentUser } from '@/lib/getCurrentUser';
-import { redirect } from 'next/navigation';
-
+import DoctorProfile from './DoctorProfile';
 export default async function DoctorDetailPage({ params }) {
   const { id } = await params;
-  const user = await getCurrentUser();
-  if(!user){
-    redirect('/users/login');
-  }
   let doctor = null;
   try {
     const data = await fetcher(`doctor/${id}`);
@@ -22,25 +15,6 @@ export default async function DoctorDetailPage({ params }) {
   if (!doctor) return <p>Doctor not found.</p>;
 
   return (
-    <main className={styles.page}>
-      <div className={styles.card}>
-        <h1>{doctor.name}</h1>
-        <p><strong>Specialization:</strong> {doctor.specialization}</p>
-        <p><strong>Email:</strong> {doctor.email}</p>
-        <p><strong>Phone:</strong> {doctor.phone}</p>
-        {doctor.availableSlots?.length > 0 ? (
-          <ul className={styles.slotList}>
-            {doctor.availableSlots.map(slot =>
-              slot.time?.map((time, idx) => (
-                <li key={`${slot._id}-${idx}`} className={styles.slotItem}>
-                  <span className={styles.slotDate}>{slot.date} </span>
-                  <span className={styles.slotTime}>{time}</span>
-                </li>
-              ))
-            )}
-          </ul>
-        ) : <p>No available slots.</p>}
-      </div>
-    </main>
+    <DoctorProfile doctor={doctor} />
   );
 }
