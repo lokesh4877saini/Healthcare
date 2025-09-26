b3b3275f1fb80f16b518c6e569f1c7da626bd154
lokesh4877saini <genu08saini@gmail.com>
2025-09-26 08:42:43 +0530

    update cookie options

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -3,12 +3,14 @@ const sendToken = (user, statusCode, res) => {
     const token = user.generateJWT();
     const isProduction = process.env.NODE_ENV === "production";
 
-const options = {
-  httpOnly: true,
-  secure: isProduction ? true : false,   //  don’t force secure in dev
-  sameSite: isProduction ? "none" : "lax", // lax works for same-origin dev
-  expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-};
+    const options = {
+        httpOnly: true,
+        //   secure: isProduction ? true : false,   //  don’t force secure in dev
+        //   sameSite: isProduction ? "none" : "lax", // lax works for same-origin dev
+        sameSite: "None",
+        secure: true,
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    };
 
     res.cookie("token", token, options).status(statusCode).json({
         success: true,
